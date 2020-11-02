7451c5c0fc8ad47a892357aa358d2f30601ef1e0
DinhThienPhuc <thienphuc.bka1412@gmail.com>
2020-11-02 16:44:08 +0700

    Refactor

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,31 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 class App extends Component {
-		constructor(props) {
-				super(props);
-				this.state = {
-						users: []
-				}
-		}
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+    };
+  }
 
-		componentDidMount() {
-				fetch('/users')
-						.then(res => res.json())
-						.then(users => this.setState({users: users}));
-		}
+  componentDidMount() {
+    fetch("/users")
+      .then((res) => res.json())
+      .then((users) => this.setState({ users: users }));
+  }
 
-		render() {
-				return (
-						<div className="App">
-								<h1>Users</h1>
-								{this
-										.state
-										.users
-										.map(user => <div key={user.id}>{user.username}</div>)}
-						</div>
-				);
-		}
+  render() {
+    return (
+      <div className="App">
+        <h1>Users</h1>
+        {this.state.users.map((user) => (
+          <div key={user.id}>{user.username}</div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
